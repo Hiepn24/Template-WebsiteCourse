e6b3b40dde5d8ceaaba3643b7f8d24ad56886edc
Hiepn24 <hiepn.240804@gmail.com>
2024-07-10 22:48:23 +0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Dùng để hiển thị các phần tử của trang web
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
